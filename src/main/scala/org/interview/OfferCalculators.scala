8522c4b57bae1943805b6fe7403c4fd67c7376a2
Rob <14052683+ralger@users.noreply.github.com>
2020-03-08 21:00:33 +0000

    Refactored OfferCalculators

--- src/main/scala/org/interview/OfferCalculators.scala
+++ src/main/scala/org/interview/OfferCalculators.scala
@@ -7,21 +7,27 @@ object OfferCalculators {
 
   def productPercentageDiscountOffer(discountProductName: ItemId,
                                      discountPercentage : Double)(basketProducts: BasketWithPrices): List[AppliedOffer] = {
-    val discountedProducts = basketProducts.filter { case (itemName, (_, _)) => itemName == discountProductName }
-    if (discountedProducts.nonEmpty) {
-      val discount = discountedProducts.map { case (_, (itemQuantity, itemPrice)) => itemPrice * itemQuantity * discountPercentage }.sum
-      AppliedOffer(f"$discountProductName ${discountPercentage * 100}%2.0f%% off", discount) :: Nil
-    } else
-      List()
+    basketProducts.get(discountProductName) match {
+      case Some((itemQuantity, itemPrice)) => {
+        val discount = itemPrice * itemQuantity * discountPercentage
+        AppliedOffer(f"$discountProductName ${discountPercentage * 100}%2.0f%% off", discount) :: Nil
+      }
+      case None => List()
+    }
   }
 
   def conditionalOnQuantityOffer(conditionalQuantity: Int,
                                  conditionalProduct: ItemId,
                                  conditionalOffer: OfferCalculator)(basketProducts: BasketWithPrices): List[AppliedOffer] = {
-    val conditionalOfferApplied = if (basketProducts.exists { case (itemName, (itemQuantity, _)) => itemName == conditionalProduct && itemQuantity >= conditionalQuantity })
-      conditionalOffer(basketProducts)
-    else
-      List()
+    val conditionalOfferApplied = basketProducts.get(conditionalProduct) match {
+      case Some((itemQuantity, _)) => {
+        if (itemQuantity >= conditionalQuantity)
+          conditionalOffer(basketProducts)
+        else
+          List()
+      }
+      case None => List()
+    }
     conditionalOfferApplied.map(offer => AppliedOffer(s"${offer.discountName} when buying $conditionalQuantity $conditionalProduct", offer.discountApplied))
   }
 
